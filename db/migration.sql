1c42d435277596bad0295a3102543a394afb489c
cam iverson <camiver@gmail.com>
2016-03-23 18:55:37 -0500

    final working version

--- db/migration.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE sinatra_coupons;
-\c sinatra_coupons
-CREATE TABLE coupons (id SERIAL PRIMARY KEY, title VARCHAR(255), description VARCHAR(255), product VARCHAR(255), discount_price DECIMAL, expiration_date DATE);
